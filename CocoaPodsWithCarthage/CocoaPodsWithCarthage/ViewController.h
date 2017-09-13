5b3df682dbbf96951c3357c58c7900c85002c518
Lucas Huang <lucas@tokbox.com>
2017-09-13 10:12:36 -0700

    init commit

---
+++ CocoaPodsWithCarthage/CocoaPodsWithCarthage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaPodsWithCarthage
+//
+//  Created by Xi Huang on 9/12/17.
+//  Copyright © 2017 LucasHuang, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CocoaPodsWithCarthage/CocoaPodsWithCarthage/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  CocoaPodsWithCarthage
+//
+//  Created by Xi Huang on 9/12/17.
+//  Copyright © 2017 LucasHuang, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <OpenTok/OpenTok.h>
+#import <JSQDataSourcesKit/JSQDataSourcesKit.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
